251e2816011e5174aa86f257c62078296f6a7845
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-07-04 22:23:48 +0300

    add menu page

---
+++ src/components/Menu.jsx
@@ -0,0 +1,33 @@
+function Menu() {
+  return (
+    <>
+      <div className="w-full flex-col border-2 h-full">
+        <div>
+          <h2 className="text-2xl font-bold p-10 border-b-2">
+            Personel Finance
+          </h2>
+        </div>
+        <div>
+          <button className=" font-bold p-3 mt-5">Dashboard</button>
+        </div>
+        <div>
+          <button className=" font-bold p-3">Analitycs</button>
+        </div>
+        <div>
+          <button className=" font-bold p-3">Dashboard</button>
+        </div>
+        <div>
+          <button className="font-bold p-3">Dashboard</button>
+        </div>
+        <div className="">
+          <button className="font-bold p-3">Dashboard</button>
+        </div>
+        <div className="w-full">
+          <button className=" font-bold p-3">Dashboard</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Menu;
