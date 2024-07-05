f4ecb31018563796bbb25f81382dd57a9b741636
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-07-05 22:38:06 +0300

    add rescharts

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import LineGraph from "./Line.jsx";
 import { useDataStore } from "./Store.jsx";
+import LineChartComponent from "./LineChart.jsx";
 function App() {
   const { totalExpenses, totalIncomes } = useDataStore((state) => ({
     expenses: state.expenses,
@@ -11,8 +12,11 @@ function App() {
   return (
     <>
       <div className="flex w-full justify-center">
-        <div className="w-2/3">
+        <div className="w-48">
           <LineGraph />
+        </div>
+        <div className="w-1/2 h-60 m-16">
+          <LineChartComponent />
         </div>
       </div>
     </>
