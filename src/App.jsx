1a6545dd39b2e18663bea97d1e2c9d66bbf6062d
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-07-07 14:35:27 +0300

    fix response design

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,14 @@ import Menu from "./components/Menu.jsx";
 function App() {
   return (
     <>
-      <div className="flex w-full justify-center p-5">
-        <div>
+      <div className="flex flex-wrap">
+        <div className="w-full sm:w-auto md:w-1/4 lg:w-1/4 xl:w-1/4 p-5">
           <Menu />
         </div>
-        <div className="w-6/7 p-5">
+        <div className="w-full sm:w-full md:w-3/4 lg:w-3/4 xl:w-3/4 p-5">
           <AddTable />
         </div>
       </div>
-      <div></div>
     </>
   );
 }
