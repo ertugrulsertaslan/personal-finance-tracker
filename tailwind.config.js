ddf5411b5f0163c4f3a824093b5dd4806509a331
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-07-09 19:51:17 +0300

    add custom color for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBgColor: "#060d20",
+        customDashboardColor: "#010416",
+      },
+    },
   },
   plugins: [],
 };
