2bc4fb32becf757e8ca7da179ca45a081f429b6d
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-07-23 23:33:08 +0300

    add custom color and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,18 @@ export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        myriad: ["Myriad"],
+      },
       colors: {
         customDashboardColor: "#010416",
         customBgColor: "#f7f8fa",
         customEmojiBgColor: "#eaf2f8",
         customRangeColor: "#ecf1f5",
         customLineColor: "#266df1",
+        customCardBgColor: "#103898",
+        customCardBtnBgColor: "#f1f8ff",
+        customCardBtnTextColor: "#0674fb",
       },
     },
   },
